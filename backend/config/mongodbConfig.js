4126b3ab15fdb4063d7b51017fe8149d60dc993a
wasimmahamod <wasimmahamodraza@gmail.com>
2024-03-24 15:09:26 +0600

    env package add

--- backend/config/mongodbConfig.js
+++ backend/config/mongodbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongodbConfig=()=>{
 
-    mongoose.connect(`mongodb+srv://sajib562341:tdOz1tOZFvBqXCQE@cluster0.ur9bur3.mongodb.net/nrivoya?retryWrites=true&w=majority&appName=Cluster0`)
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.ur9bur3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => console.log('Connected!'));
 
 }
