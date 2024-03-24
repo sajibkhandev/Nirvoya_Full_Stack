1a7b28b190069c596a532d7c2673a556b5f7a20e
wasimmahamod <wasimmahamodraza@gmail.com>
2024-03-24 16:31:07 +0600

    helper add

--- backend/controllers/registrationController.js
+++ backend/controllers/registrationController.js
@@ -1,4 +1,16 @@
+const blankInput = require("../helpers/blankInput");
+
 const registrationController=(req,res)=>{
+
+    const {username,email,password}=req.body
+    if(blankInput(username)){
+        res.send({error:"user name is required"})
+    }else if(blankInput(email)){
+        res.send({error:"email is required"})
+    }else if(blankInput(password)){
+        res.send({error:"password is required"})
+    }
+
     res.send(req.body);
     
 }
---
+++ backend/helpers/blankInput.js
@@ -0,0 +1,8 @@
+const blankInput=(text)=>{
+    if(!text){
+        return true
+    }else{
+        return false
+    }
+}
+module.exports=blankInput
