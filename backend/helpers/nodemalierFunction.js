c26a6fb70ee1fa35eea38d4a906c3fde2f634f21
sajibkhandev <sajibkhan.mern@gmail.com>
2024-03-28 10:29:55 +0600

    nodemailer email,pass add in env

--- backend/helpers/nodemalierFunction.js
+++ backend/helpers/nodemalierFunction.js
@@ -4,15 +4,15 @@ const nodemailer = require("nodemailer");
         service: "gmail",
         secure: false, 
         auth: {
-          user: "sajib562341@gmail.com",
-          pass: "mmjz sdli yzin rewl",
+          user: process.env.NODE_MAILER_USER_EMAIL,
+          pass: process.env.NODE_MAILER_USER_PASSWORD,
         },
       });
 
 
       function sendEmail(email,subject,html){
         return  transporter.sendMail({
-        from: 'sajib562341@gmail.com', // sender address
+        from: process.env.NODE_MAILER_USER_EMAIL, // sender address
         to: email, // list of receivers
         subject: subject, // Subject line
         html: html, // html body
