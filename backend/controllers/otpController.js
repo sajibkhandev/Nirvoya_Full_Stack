e66ca9881087353e71cd4f09b5eeead308a3640a
sajibkhandev <sajibkhan.mern@gmail.com>
2024-03-30 10:34:15 +0600

    paramiter pass in otpController

--- backend/controllers/otpController.js
+++ backend/controllers/otpController.js
@@ -3,11 +3,13 @@ const userSchema = require("../models/userSchema");
 const otpController=async(req,res)=>{
     const{email,otp}=req.body
     let otpFind=await userSchema.findOne({email:email})
-    if(otpFind.otp==otp){
+    if(otpFind){
+        if(otpFind.otp==otp){
         await userSchema.findOneAndUpdate({email:email},{otp:""})
         res.send({success:"Email Verified"})
     }else{
         res.send({error:"Otp not match"})
+    }
     }
 
 }
