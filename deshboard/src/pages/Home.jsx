867bbe4e1c862c2e872ca32026df1ffcd156442b
sajibkhandev <sajibkhan.mern@gmail.com>
2024-03-30 10:57:25 +0600

    react router dom add

---
+++ deshboard/src/pages/Home.jsx
@@ -0,0 +1,97 @@
+import { Button, Form, Input } from 'antd';
+import axios from 'axios'
+
+const Home = () => {
+     const onFinish = async(values) => {
+
+    let data=await axios.post('http://localhost:8000/api/v1/auth/registration',{
+      username:values.username,
+      email:values.email,
+      password:values.password
+
+    },{
+      headers:{
+        Authorization: "ertertgrfgvfdf"
+      }
+    }
+    )
+    console.log(data);
+    
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+  return (
+     <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+     <Form.Item
+      label="Email"
+      name="email"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your email!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+  )
+}
+
+export default Home
