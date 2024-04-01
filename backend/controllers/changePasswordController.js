0291abec5a454059a3d42cf8eb4267f8e04d2e1d
sajibkhandev <sajibkhan.mern@gmail.com>
2024-04-01 09:42:06 +0600

    chanage password bcrypt

--- backend/controllers/changePasswordController.js
+++ backend/controllers/changePasswordController.js
@@ -1,12 +1,16 @@
 const userSchema = require("../models/userSchema");
 const nodemailer = require("nodemailer");
 var jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-const changePasswordController=async(req,res)=>{
+const changePasswordController=(req,res)=>{
     const {token,password}=req.body
     var decoded = jwt.verify(token, 'sajib');
+    bcrypt.hash(password, 10, async function(err, hash) {
+    await userSchema.findOneAndUpdate({email:decoded.email},{password:hash})
     
-    await userSchema.findOneAndUpdate({email:decoded.email},{password:password})
+});
+    
 
 }
 module.exports=changePasswordController
