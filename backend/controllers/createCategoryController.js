2acddd0dc9d4049a2d613c731e9fce87da6c638a
sajibkhandev <sajibkhan.mern@gmail.com>
2024-04-02 13:11:53 +0600

    add product in creatCategory

--- backend/controllers/createCategoryController.js
+++ backend/controllers/createCategoryController.js
@@ -3,8 +3,18 @@ const CatSchema = require("../models/catSchema");
 const createCategoryController=async(req,res)=>{
 
     let {name,ownerId}=req.body
-   console.log(name);
-   console.log(ownerId);
+    const existsName=await CatSchema.findOne({name:name})
+    if(existsName){
+        res.send({error:"already exists this product"})
+
+    }else{
+        let makeProduct=new CatSchema({
+            name:name,
+            ownerId:ownerId
+        })
+        makeProduct.save()
+        res.send({success:"product has make but waiting for admin approval"})
+    }
 
 }
 module.exports=createCategoryController
