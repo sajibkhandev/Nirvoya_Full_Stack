ac50a349932a192e27291ea935f84f39ec5dd610
sajibkhandev <sajibkhan.mern@gmail.com>
2024-05-15 16:31:41 +0600

    category page add

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+      <h1>Navber</h1>
+        {children}
+        <h1>Banner</h1>
+        </body>
     </html>
   );
 }
---
+++ frontend/components/category.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+async function getData() {
+    const res = await fetch('http://localhost:8000/api/v1/product/allCategory')
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    return res.json()
+  }
+
+const Category = async() => {
+    const data = await getData()
+    console.log(data);
+  return (
+    <div>
+        {data.map(item=>(
+            <li><Link href={`category/${item.name}`}>{item.name}</Link></li>
+        ))}
+    </div>
+  )
+}
+
+export default Category
