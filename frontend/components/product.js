19e48a7583b052c808a369dbd97e21a50a7c057c
sajibkhandev <sajibkhan.mern@gmail.com>
2024-05-18 19:30:39 +0600

    product show in frontend

---
+++ frontend/components/product.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+async function getData() {
+    const res = await fetch('http://localhost:8000/api/v1/product/allProduct')
+
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+const Product = async () => {
+    const data = await getData()
+    console.log(data);
+
+    return (
+        <div>
+            {data.map(item => (
+                <>
+                    <li>{item.name}</li>
+                    <li>
+                        <Image
+                            src={`http://localhost:8000${item.image}`}
+                            width={250}
+                            height={250}
+                            alt="Picture of the author"
+                        />
+                    </li>
+                    <li>{item.regularprice?<span><del><span>{item.regularprice}</span></del> <span>{item.sellprice}</span></span>:<span>{item.sellprice}</span>}</li>
+                </>
+            ))}
+        </div>
+    )
+}
+
+export default Product
