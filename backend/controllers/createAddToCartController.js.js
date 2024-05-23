a1b15a294549bdbb32906760b60e9fa2888b2949
sajibkhandev <sajibkhan.mern@gmail.com>
2024-05-23 17:27:15 +0600

    Add to cart created using backend

--- backend/controllers/createAddToCartController.js.js
+++ backend/controllers/createAddToCartController.js.js
@@ -2,7 +2,19 @@ const AddToCart = require("../models/addToCartSchema");
 
 const createAddToCartController=async(req,res)=>{
     let {productId,quantity,cartOwnerId}=req.body
-    console.log(productId,quantity,cartOwnerId);
+    let existingData=await AddToCart.find({productId:productId})
+    if(existingData.length>0){
+        let data2= await AddToCart.findOneAndUpdate({_id:existingData[0]._id},{quantity:existingData[0].quantity+1},{new:true})
+        console.log(data2);
+    }else{
+        let data=new AddToCart({
+            productId:productId,
+            quantity:1,
+            cartOwnerId:"6645ff1f3504cf890f74dec4"
+          })
+          data.save()
+        res.send({status:"done"})
+    }
 
    
 
