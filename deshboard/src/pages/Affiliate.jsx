6500534fe9a2720a79f40aa74d3502cf79ebe8ca
sajibkhandev <sajibkhan.mern@gmail.com>
2024-06-02 16:50:27 +0600

    allifiate link add

--- deshboard/src/pages/Affiliate.jsx
+++ deshboard/src/pages/Affiliate.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {useSelector} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 const Affiliate = () => {
+  let [allData,setAllData]=useState([])
+  let user=useSelector((state)=>(state.currentUser.value.id))
+  console.log(user);
+  
+  useEffect(()=>{
+
+    async function allProduct(){
+      let data=await axios.get("http://localhost:8000/api/v1/product/allProduct")
+      setAllData(data.data);
+
+    }
+    allProduct()
+
+  },[])
   return (
-    <div>Affiliate</div>
+   <>
+   { allData.map(item=>(
+      <li style={{borderBottom:"1px solid red",margin:"20px",padding:"10px"}}>{item.name}--------- <Link to={`http://localhost:3000/product/${item.slug}?userId=${user}`}>{`http://localhost:3000/product/${item.slug}?userId=${user}`}</Link></li>
+    
+    ))}
+   </>
   )
 }
 
